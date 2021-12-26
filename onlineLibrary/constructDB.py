b457626bcb11453e2c035dc366a04a0d6727e5a6
Lounes.Brahimi <lounes.brahimi@etu.sorbonne-universite.fr>
2021-12-26 15:49:30 +0100

    Parcourt les livre de l'api et ajoute seulement ceux qui correspondent au bon critere de la base de donnee

--- onlineLibrary/constructDB.py
+++ onlineLibrary/constructDB.py
@@ -1,23 +1,29 @@
 import urllib.request
 
+def numberWordOfString(text : str):
+    word_list = text.split()
+    return len(word_list)
+
 def contructDB(bookId: int, sizeDB: int, minNumberWords: int):
     numberBooksStocked : int = 0
+    textBook : str = ""
     while (numberBooksStocked < sizeDB):
         bookIdStr = str(bookId)
         try:
             with urllib.request.urlopen("https://gutenberg.org/files/"+bookIdStr +"/"+
                                 bookIdStr+"-h/"+bookIdStr+"-h.htm") as url:
-                textBook : str = url.read().decode('utf-8')
-                print(len(textBook))
+                textBook = url.read().decode('utf-8')
+                numberWords : int = numberWordOfString(textBook)
+                if (numberWords >= minNumberWords):
+                    numberBooksStocked += 1
+                    print(numberWords)
+                    print(len(textBook))
         except:
             pass
-        numberBooksStocked += 1
         bookId += 1
 
-        
 
-
-sizeDB : int = 1664
-minNumberWords : int = 10006
-bookInitialId : int = 56672
+sizeDB : int = 10
+minNumberWords : int = 10000
+bookInitialId : int = 56667
 contructDB(bookInitialId, sizeDB, bookInitialId)
